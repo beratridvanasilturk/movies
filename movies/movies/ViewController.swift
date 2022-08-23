a892bb5c9e5dad029658a4c8a6db5f1cffc5a4f0
beratridvanasilturk <br.asltrk@gmail.com>
2022-08-23 17:26:00 +0300

    init

---
+++ movies/movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movies
+//
+//  Created by Berat Rıdvan Asiltürk on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
