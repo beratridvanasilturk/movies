1b86aa299c93a15fd6accf0caa71507402a1d4b7
beratridvanasilturk <br.asltrk@gmail.com>
2022-08-24 17:52:28 +0300

    add Kingfisher

--- movies/movies/ViewControllers/FilmsViewController.swift
+++ movies/movies/ViewControllers/FilmsViewController.swift
@@ -6,10 +6,18 @@
 //
 
 import UIKit
+import Kingfisher
 
 class FilmsViewController: UIViewController {
 
     @IBOutlet weak var filmsTableView: UITableView!
+
+    let films = [
+        "https://i.pinimg.com/736x/6c/e6/7f/6ce67f7b4fe02b66dc06187ed658000f--serena--bradley-cooper.jpg",
+        "https://www.hdmekani.com/rsm/images/56308041452025876161.jpg",
+        "https://tr.web.img2.acsta.net/medias/nmedia/18/89/83/45/20175397.jpg",
+        "https://cdn-0.tvprofil.com/img/covers/img266041-tv2457493.jpg",
+        "https://pbs.twimg.com/media/CMX5xkRWgAAEggJ.png:large"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,26 +30,23 @@ class FilmsViewController: UIViewController {
 }
 
 extension FilmsViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
+    }
 }
 
 extension FilmsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return films.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "FilmsCell", for: indexPath) as! FilmTableViewCell
         
-        
-        let imageName = ["a","b","c","d","e","f","g","h","j","theGodfather"].randomElement() ?? ""
-        
-        cell.filmImageView?.image = UIImage(named: imageName)
-        
+        let url = URL(string: films[indexPath.row])!
+        cell.filmImageView?.kf.setImage(with: url)
         
         return cell
     }
-    
-    
 }
